30b05359fa2e5729516db2796778ec1fb800b110
Daniel Vera Morales <dvera@sunset.com.mx>
2017-07-25 10:45:28 -0500

    Add: Recursive upload

--- config/cfgtmpignore.sh
+++
@@ -1,1 +0,0 @@
-sourceFile=crm4marketingsunset-1.0-SNAPSHOT.notstable.jar
--- lib/mod/uploadjar.sh
+++ lib/mod/uploadjar.sh
@@ -2,8 +2,14 @@
 source lib/head.sh
 
 function uploadJ(){
+	if [[ -z "$uploadDirectory" ]] || [[ $uploadDirectory == 0 ]]; then
+		uploadDirectory="";
+		else
+		uploadDirectory="-r"
+	fi
+	
 	echo "====================================";
 	echo "[+] UPLOADING "$sourceFile" TO SERVER";
 	echo "====================================";
-	scp -P $port $sourcePath$sourceFile $user"@"$host":"$destPath;$1;
+	scp $uploadDirectory -P $port $sourcePath$sourceFile $user"@"$host":"$destPath;$1;
 }
