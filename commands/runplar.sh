1939f88ac6c272c88b449d4240a85d088b9dcc2e
Daniel V. Morales <dvera@sunset.com.mx>
2017-10-06 12:59:28 -0500

    Remove: Remove bad dependencies

--- commands/runplar.sh
+++ commands/runplar.sh
@@ -1,1 +1,7 @@
-runcommand="cd "$destPath";sudo  "$nohupable"  /usr/sbin/Plar/plarx run -f  "$destPath"  -n "$kewyword
+
+if [ -z "$play_arg" ]; then
+	play_arg="export USING=run;";
+	else
+	play_arg="export USING=${play_arg};";
+fi;
+runcommand=$play_arg"cd "$destPath";sudo  "$nohupable"  /usr/sbin/Plar/plarx rclean -f  "$destPath"  -n "$kewyword
